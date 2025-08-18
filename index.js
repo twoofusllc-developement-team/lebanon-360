bf1ad5a828ca975f01c68c9e75e6f5eaba074139
Rana Kiwan <rana.kiwan1@gmail.com>
2025-08-18 18:29:59 +0300

    fixed index

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 const DB = require('./database').connectDB; 
 const offeringRoutes = require('./routes/offeringRoutes');
 const personRoutes = require('./routers/personRoutes');
+const storyRoutes = require('./routers/storyRoutes');
+const offeringRoutes1 = require('./routers/postofferingroute');
+const getOfferingRoutes = require('./routers/getOfferingRoute');
 
 app.use(express.json()); 
 
 app.use('/api/stories', storyRoutes);
+app.use('/api', offeringRoutes1);
 
  app.use('/api/offerings', offeringRoutes);
+app.use('/api', getOfferingRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
