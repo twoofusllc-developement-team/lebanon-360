9b9b8feef5b68574116325d7a91c371591e2bc96
Rana Kiwan <rana.kiwan1@gmail.com>
2025-08-18 13:08:06 +0300

    fixed db.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const DB = require('./database').connectDB; 
+DB();
+app.use(express.json());
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
 const offeringRoutes = require('./routes/offeringRoutes');
 const storyRoutes = require('./routers/storyRoutes');
 
-DB(); // Connect to MongoDB
 
-app.use(express.json()); 
 
 app.use('/api/stories', storyRoutes);
 
